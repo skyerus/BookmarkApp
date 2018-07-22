8c134df8bf0371fa1922997f3e339f42986da46b
Skye Gill <sg802@bath.ac.uk>
2018-07-22 20:24:00 +0100

    add bookmark icon

--- src/components/AddBookmark.js
+++ src/components/AddBookmark.js
@@ -3,8 +3,7 @@ import plus from '../img/plus.svg';
 
 export default () => {
   return (
-    <div className="nested-bookmark-element">
-        <h1 className="plus-header">Bookmark</h1>
+    <div className="plus-bookmark-element">
         <div className="plus-container">
             <img src={plus} className="plus-img" alt="Add a new bookmark"/>
         </div>
--- src/components/Bookmarks.js
+++ src/components/Bookmarks.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 
 import Bookmark from './Bookmark';
 import AddBookmark from './AddBookmark';
-import AddCategory from './AddCategory';
 
 export default class Bookmarks extends Component {
   render() {
@@ -11,7 +10,7 @@ export default class Bookmarks extends Component {
             edit={this.props.edit} 
             key={this.props.bookmarks[order].id} 
             name= {this.props.bookmarks[order].title + this.props.bookmarks[order].id} 
-            about= "This is a bookmark" 
+            about= "This is a bookmark iyhfi ues aieuh eiu hefi huef hiue fhiu efs hiue sfi ue sfaf  ssfe ffefea ffaf afa faa afaf afaf af" 
             reorderBookmarks={this.props.reorderBookmarks} 
             order={order} 
             index={index}
@@ -20,14 +19,12 @@ export default class Bookmarks extends Component {
     return (
         <div className="my-container min-height my-modal bookmarks-modal">
             <div className="bookmark-header-container">
-                <div className="left-grid-btn"><button className="btn btn-dark">New</button></div>
                 <div className="left-grid-btn"><button className="btn btn-dark" onClick={this.props.toggleEdit}>Edit</button></div>
                 <div className="title-grid-element"><h1 className="padding-top-h1">Bookmarks</h1></div>
                 <div className="save-grid-btn"><button className="btn btn-dark">Save</button></div>
             </div>
             <div className="bookmark-grid">
                 <AddBookmark/>
-                <AddCategory/>
                 {bookmarks}
             </div>
         </div>
